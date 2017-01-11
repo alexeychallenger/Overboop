542ca3f9a76e24bf693c6f66b1022e0aba27a3e4
Alexey Arefjev <alexeychallenger@gmail.com>
2017-01-11 15:10:32 +0200

    изменения клавиш управления камерой
    добавлено назначение клавиш для управления игровой камерой

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,11 @@ public class CameraController : MonoBehaviour {
 
     //контроллер игровой камеры
 
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
     private Camera camera;  //ссылка камеру
     
     [Header ("Stats")]
@@ -20,30 +25,30 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        MoveAD();       //вызов функции движения по оси Х
-        MoveWS();       //вызов функции движения по оси У
+        MoveX();       //вызов функции движения по оси Х
+        MoveY();       //вызов функции движения по оси У
     }
     
 
-    private void MoveAD()                       //перемешение влево-вправо (ось Х)
+    private void MoveX()                       //перемешение влево-вправо (ось Х)
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(leftKey))
         {
             camera.transform.Translate(new Vector3(-speed, 0, 0));
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(rightKey))
         {
             camera.transform.Translate(new Vector3(speed, 0, 0));
         }
     }
 
-    private void MoveWS()                       //перемешение вверх-вниз  (ось У)
+    private void MoveY()                       //перемешение вверх-вниз  (ось У)
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(upKey))
         {
             camera.transform.Translate(new Vector3(0, speed, 0));
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(downKey))
         {
             camera.transform.Translate(new Vector3(0, -speed, 0));
         }
